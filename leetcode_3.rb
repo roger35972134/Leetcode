2b7593dbf74acaaf4a3a6c14302b9cb2654ef9a2
roger <roger35972134@gmail.com>
2017-10-11 13:55:10 +0800

    add question 3

---
+++ leetcode_3.rb
@@ -0,0 +1,20 @@
+# @param {String} s
+# @return {Integer}
+def length_of_longest_substring(s)    
+    longest_length = 0
+    for i in 0..s.length-1
+        check = []
+        for j in i..s.length-1
+            unless check.include? s[j]
+                check.push s[j]
+            else
+                break
+            end
+        end
+        if check.size > longest_length
+            longest_length = check.size
+            index = i
+        end
+    end
+    longest_length
+end
