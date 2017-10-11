b021083fa1ba09d4fe9d63299a08ba79cfba0917
roger <roger35972134@gmail.com>
2017-10-11 17:40:07 +0800

    add Leetcode_3:Accepted

---
+++ leetcode_3:Accepted.rb
@@ -0,0 +1,17 @@
+def length_of_longest_substring(s) 
+    max = 0
+    start_index = -1
+    char_to_index_map = {}
+    
+    s.chars.each_with_index do |c, i|
+        if current_index = char_to_index_map[c] and current_index > start_index # if current_index != nil
+            start_index = current_index
+        end
+        
+        char_to_index_map[c] = i
+        new_length = i - start_index
+        max = new_length if max < new_length
+        
+    end
+    max
+end
